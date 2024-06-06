bf62b2e534a055b058b4e8132b4471161d4f002a
Fernando Gonzalez <gonzalez0fer@gmail.com>
2024-06-06 17:22:07 -0400

    feat: pokedash plotly chart

---
+++ README.md
@@ -0,0 +1,72 @@
+# Poke-Dash: Interactive Pokémon Data Visualization Dashboard
+
+Poke-Dash is an interactive data visualization dashboard built using Plotly, Dash, and pandas. This project demonstrates how to create a dynamic and responsive web application to explore and visualize data on the first 150 Pokémon. With Poke-Dash, you can filter Pokémon by type, select various metrics to display, and choose different types of charts to visualize the data.
+
+## Features
+
+- **Interactive Charts**: Poke-Dash offers a variety of interactive charts, including bar charts, line charts, scatter plots, and more. Users can explore data in an intuitive and engaging way.
+- **Responsive Design**: The dashboard is fully responsive, ensuring a seamless experience across different devices, including desktops, tablets, and smartphones.
+- **Customizable Style**: Utilizing CSS and Dash's built-in theming, the dashboard features a modern, dark theme with customizable styles.
+- **Dynamic Filtering**: Users can filter Pokémon by type and select various metrics to display, allowing for detailed data exploration.
+
+## Technologies Used
+
+- **Dash**: A Python framework for building analytical web applications.
+- **Plotly**: A graphing library that makes interactive, publication-quality graphs online.
+- **pandas**: A data analysis and manipulation tool.
+- **HTML/CSS**: For layout and styling of the web application.
+
+## Installation
+
+1. Clone the repository
+
+2. Change to the project directory
+
+3. Create a virtual environment and activate it
+```
+python3 -m venv venv
+source venv/bin/activate  # On Windows use `venv\Scripts\activate`
+```
+
+4. Install the required dependencies
+```
+pip install -r requirements.txt
+```
+
+
+## Usage
+
+1. Ensure you have the Pokémon data CSV file in the data directory.
+
+2. Run the Dash application:
+python app.py
+
+3. Open your web browser and go to http://127.0.0.1:8050 to view the dashboard.
+
+## Project Structure
+- **app.py**: The main application file containing the Dash app configuration and callback functions.
+- **data/pokemon.csv**: The CSV file containing data on the first 150 Pokémon.
+- **assets/styles.css**: Custom CSS styles for the application.
+- **assets/banner.jpg**: The main banner image.
+- **assets/title.jpg**: The title image used in the dashboard.
+
+
+## Customization
+- CSS Styling: Modify assets/styles.css to change the look and feel of the dashboard.
+- Data: Update data/pokemon.csv with new data if needed.
+- Chart Types: The available chart types are defined in the chart_functions dictionary in app.py. Add or remove chart types as needed.
+
+# Example Dashboard
+Here is a preview of the Poke-Dash dashboard:
+
+
+## Contributing
+Contributions are welcome! Please fork this repository and submit a pull request with your changes.
+
+## License
+This project is licensed under the MIT License. See the LICENSE file for more details.
+
+## Acknowledgments
+- Plotly for the amazing graphing library.
+- Dash for the powerful web application framework.
+- pandas for data manipulation.
---
+++ app.py
@@ -0,0 +1,182 @@
+import pandas as pd
+import plotly.express as px
+from dash import Dash, Input, Output, callback, dcc, html
+
+"""
+Load the dataset and initialize the Dash application.
+
+Attributes:
+    df (DataFrame): DataFrame containing Pokémon data loaded from CSV file.
+    app (Dash): Dash application instance.
+"""
+df = pd.read_csv("data/pokemon.csv")
+
+app = Dash(__name__)
+
+"""
+Define the layout of the Dash application.
+
+Components:
+    html.H1: Title of the dashboard.
+    dcc.Dropdown: Dropdown for selecting Pokémon type.
+    dcc.Dropdown: Dropdown for selecting metric to display.
+    dcc.Dropdown: Dropdown for selecting chart type.
+    dcc.Graph: Graph component to display the selected chart.
+"""
+app.layout = html.Div(
+    [
+        html.Div(
+            [
+            html.Img(src='/assets/banner.jpg', className='blurry-image'),
+            html.Img(src='/assets/pokedash.png', className='title'),
+            ]
+        ),
+        html.H1(className='space'),
+        html.P(children="select your Pokemon type:"),
+        dcc.Dropdown(
+            id="dropdown-type-selection",
+            options=[{"label": type_, "value": type_} for type_ in df.type.unique()],
+            value="Grass",
+        ),
+        html.P(children="select your Pokemon stat:"),
+        dcc.Dropdown(
+            id="dropdown-metric-selection",
+            options=[
+                {"label": "HP", "value": "hp"},
+                {"label": "Attack", "value": "attack"},
+                {"label": "Defense", "value": "defense"},
+                {"label": "Speed", "value": "speed"},
+                {"label": "Special Attack", "value": "sp_attack"},
+                {"label": "Special Defense", "value": "sp_defense"},
+                {"label": "Total", "value": "total"},
+            ],
+            value="hp",
+        ),
+        html.P(children="select your chart type:"),
+        dcc.Dropdown(
+            id="dropdown-chart-type",
+            options=[
+                {"label": "Bar", "value": "bar"},
+                {"label": "Line", "value": "line"},
+                {"label": "Scatter", "value": "scatter"},
+                {"label": "Histogram", "value": "histogram"},
+                {"label": "Box", "value": "box"},
+                {"label": "Violin", "value": "violin"},
+                {"label": "Pie", "value": "pie"},
+                {"label": "Sunburst", "value": "sunburst"},
+                {"label": "Treemap", "value": "treemap"},
+                {"label": "Heatmap", "value": "heatmap"},
+                {"label": "Density Contour", "value": "density_contour"},
+                {"label": "Area", "value": "area"},
+                {"label": "Funnel", "value": "funnel"},
+                {"label": "Polar Line", "value": "line_polar"},
+                {"label": "Polar Scatter", "value": "scatter_polar"},
+                {"label": "Polar Bar", "value": "bar_polar"},
+            ],
+            value="bar",
+        ),
+        dcc.Graph(id="graph-content"),
+    ]
+)
+
+"""
+Dictionary mapping chart types to Plotly Express functions.
+
+Keys:
+    str: Chart type name.
+Values:
+    function: Corresponding Plotly Express function.
+"""
+chart_functions = {
+    "bar": px.bar,
+    "line": px.line,
+    "scatter": px.scatter,
+    "histogram": px.histogram,
+    "box": px.box,
+    "violin": px.violin,
+    "pie": px.pie,
+    "sunburst": px.sunburst,
+    "treemap": px.treemap,
+    "heatmap": px.density_heatmap,
+    "density_contour": px.density_contour,
+    "area": px.area,
+    "funnel": px.funnel,
+    "line_polar": px.line_polar,
+    "scatter_polar": px.scatter_polar,
+    "bar_polar": px.bar_polar,
+}
+
+
+@callback(
+    Output("graph-content", "figure"),
+    [
+        Input("dropdown-type-selection", "value"),
+        Input("dropdown-metric-selection", "value"),
+        Input("dropdown-chart-type", "value"),
+    ],
+)
+def update_graph(selected_type, selected_metric, chart_type):
+    """
+    Callback function to update the graph based on user selections.
+
+    Args:
+        selected_type (str): Selected Pokémon type from the dropdown.
+        selected_metric (str): Selected metric to display on the y-axis.
+        chart_type (str): Selected type of chart to display.
+
+    Returns:
+        Figure: Plotly figure object representing the selected chart.
+
+    Description:
+        The function filters the DataFrame based on the selected Pokémon type,
+        determines the appropriate Plotly Express function to call based on the selected
+        chart type, and generates the corresponding chart. The chart is then returned
+        to be displayed in the Graph component.
+    """
+    if selected_type is None:
+        selected_type = df['type'].unique()[0]
+    if selected_metric is None:
+        selected_metric = 'hp'
+    if chart_type is None:
+        chart_type = 'bar'
+
+    dff = df[df.type == selected_type]
+    chart_function = chart_functions[chart_type]
+
+    if chart_type in ["pie", "sunburst", "treemap"]:
+        fig = chart_function(
+            dff,
+            names="name",
+            values=selected_metric,
+            title=f"{selected_metric.capitalize()} of {selected_type} Type Pokémon",
+        )
+    elif chart_type in ["line_polar", "scatter_polar", "bar_polar"]:
+        fig = chart_function(
+            dff,
+            r=selected_metric,
+            theta="name",
+            title=f"{selected_metric.capitalize()} of {selected_type} Type Pokémon",
+        )
+    else:
+        fig = chart_function(
+            dff,
+            x="name",
+            y=selected_metric,
+            title=f"{selected_metric.capitalize()} of {selected_type} Type Pokémon",
+        )
+
+    return fig
+
+
+if __name__ == "__main__":
+    """
+    Run the Dash application.
+
+    Args:
+        debug (bool): If True, enable debug mode.
+
+    Description:
+        This block checks if the script is run directly (not imported as a module),
+        and if so, runs the Dash application with debug mode enabled.
+    """
+    app.run(debug=True)
